61baee5b9f3c0fc0f88b4a5dc4473b28f1406d05
wakatobi-update <mencipura@gmail.com>
2026-03-16 20:28:27 +0900

    Update qr.js

--- js/qr.js
+++ js/qr.js
@@ -1,1 +1,31 @@
+// ===============================
+// GENERATE QR CODE
+// ===============================
 
+document.addEventListener("DOMContentLoaded", generateQR);
+
+
+function generateQR(){
+
+const qrContainer = document.getElementById("qrcode");
+
+if(!qrContainer) return;
+
+
+// URL halaman saat ini
+const storeURL = window.location.href;
+
+
+// generate QR
+new QRCode(qrContainer, {
+
+text: storeURL,
+width: 180,
+height: 180,
+colorDark: "#000000",
+colorLight: "#ffffff",
+correctLevel: QRCode.CorrectLevel.H
+
+});
+
+}
