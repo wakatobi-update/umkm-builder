cfed3be7809c892b4b3701181f2c3b124551a125
wakatobi-update <mencipura@gmail.com>
2026-03-16 20:19:22 +0900

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,1 +1,56 @@
+// ===============================
+// LOAD STORE DATA
+// ===============================
 
+document.addEventListener("DOMContentLoaded", loadStore);
+
+
+function loadStore(){
+
+const data = JSON.parse(localStorage.getItem("umkmStore"));
+
+if(!data) return;
+
+
+// ELEMENT
+
+const storeName = document.getElementById("storeName");
+const storeTitle = document.getElementById("storeTitle");
+const storeDescription = document.getElementById("storeDescription");
+const storeLogo = document.getElementById("storeLogo");
+const whatsappBtn = document.getElementById("whatsappBtn");
+
+
+// SET DATA
+
+storeName.textContent = data.name;
+storeTitle.textContent = data.name;
+storeDescription.textContent = data.description;
+
+
+// LOGO
+
+if(data.logo){
+
+storeLogo.src = data.logo;
+
+}else{
+
+storeLogo.style.display = "none";
+
+}
+
+
+// WHATSAPP LINK
+
+if(data.whatsapp){
+
+whatsappBtn.href = "https://wa.me/" + data.whatsapp;
+
+}else{
+
+whatsappBtn.style.display = "none";
+
+}
+
+}
